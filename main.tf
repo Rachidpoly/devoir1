1839b5997b3d62da01081d183bf46baad353ff58
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-02-21 10:33:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "example" {
 # Création du réseau virtuel
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  address_space       = ["10.0.1.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
