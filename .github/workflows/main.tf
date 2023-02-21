57c8403530420a226504813046ae9510a2191949
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-02-20 19:27:42 -0500

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -3,18 +3,18 @@
 terraform {
 backend "remote" {
 #         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
+        organization = "rachidpoly"
 #
 #         # The name of the Terraform Cloud workspace to store Terraform state files in.
 workspaces {
-          name = "example-workspace"
+          name = "rachidworkplace"
         }
        }
     }
 
 #     # An example resource that does nothing.
-   resource "null_resource" "example" {
-   triggers = {
-     value = "A example resource that does nothing!"
+ #  resource "null_resource" "example" {
+ # triggers = {
+ #    value = "A example resource that does nothing!"
       }
-     }
+ #    }
