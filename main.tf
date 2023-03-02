39a2d408d27025bfbb600c88d5b19c4bc7a04ffb
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 23:34:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,10 +58,6 @@ resource "azurerm_linux_virtual_machine" "rg" {
     azurerm_network_interface.rg.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
