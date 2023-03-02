581e1ade173bd9b59521f49d43c673ff57c908b5
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 23:47:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,37 +28,36 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  }
+}
 
-resource "azurerm_subnet" "rg" {
+resource "azurerm_subnet" "subnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "rg" {
+resource "azurerm_network_interface" "nic" {
   name                = "rg-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.rg.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "rg" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                = "Devoir1RachidVM"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.rg.id,
+    azurerm_network_interface.nic.id,
   ]
-
 
   os_disk {
     caching              = "ReadWrite"
