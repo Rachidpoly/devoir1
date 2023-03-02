11ec8cf6b139dcf605147c1a0f833ad23705a4c5
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 22:56:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "Devoir1RachidVM"
+  location              = azurerm_resource_group.EASTUS.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = []
   vm_size               = "Standard_DS1_v2"
