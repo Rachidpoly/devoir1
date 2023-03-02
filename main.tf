937f0be2cac24d4d89312825ac2f34a354fd899d
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-02 00:39:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_container_group" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "rachiddnsdevoir1cr460"
   os_type             = "Linux"
 
   container {
