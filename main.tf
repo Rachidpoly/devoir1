a8c93393ac84e48c1f39f8446d0913a49b68217c
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 23:10:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "Devoir1RachidVN"
   address_space       = ["10.0.0.0/16"]
-  location = "East US"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   }
 
