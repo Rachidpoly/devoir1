ccae75f7fe8a5d73d13acc594d869012d16d86ba
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 23:33:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,69 +19,59 @@ provider "azurerm" {
 # Define an example resource group using the "azurerm_resource_group" resource type.
 resource "azurerm_resource_group" "rg" {
   name     = "CR460Devoir1Rachid"
-  location = "East US"
+  location = "West Europe"
 }
 
-# Création du réseau virtuel
-resource "azurerm_virtual_network" "vnet" {
-  name                = "Devoir1RachidVN"
+resource "azurerm_virtual_network" "rg" {
+  name                = "rg-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  }
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
-resource "azurerm_network_interface" "example" {
-  name                = "my-nic"
+
+resource "azurerm_subnet" "rg" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.rg.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "rg" {
+  name                = "rg-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "my-ip-config"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.rg.id
     private_ip_address_allocation = "Dynamic"
   }
 }
-resource "azurerm_virtual_machine" "main" {
-  name                  = "Devoir1RachidVM"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = []
-  vm_size               = "Standard_DS1_v2"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+resource "azurerm_linux_virtual_machine" "rg" {
+  name                = "rg-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.rg.id,
+  ]
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
-  storage_image_reference {
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
 }
-
