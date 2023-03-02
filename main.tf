35f7c7f5aa53cdc83b1ad13f8fb2c2b8619f6613
Rachidpoly <124323184+Rachidpoly@users.noreply.github.com>
2023-03-01 22:54:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,9 +34,8 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "Devoir1RachidVM"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = []
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
